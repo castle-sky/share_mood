7e45e6e253071b9584dcb1ae0825ab96c7d1261e
castle-sky <familyboat@163.com>
2021-03-26 14:55:06 +0800

    finish sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
   def new
 		@user = User.new
   end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save?
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.attributes()
+		redirect_to @user
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to 'index'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'signup', to: 'users#new'
   root 'static_pages#home'
 
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+
+  get 'signup', to: 'users#new'
+	resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
